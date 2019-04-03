7f0f02495354fac8a24180b1eab62636dcd03eb7
skagnihotri <skagnihotri1@gmail.com>
2019-04-03 12:43:38 +0530

    building ann

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/sample.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/sample.py
@@ -4,3 +4,38 @@ Created on Wed Apr  3 11:37:50 2019
 
 @author: Shubham
 """
+#ANN 
+
+#part1 (data preprocessing)
+#libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:,3:-1].values
+y = dataset.iloc[:,-1].values
+
+#categories
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,1] = labelencoder_X.fit_transform(X[:,1])
+labelencoder_X1 = LabelEncoder()
+X[:,2] = labelencoder_X1.fit_transform(X[:,2])
+onehotencoder = OneHotEncoder(categorical_features=[1], categories=None)
+X = onehotencoder.fit_transform(X).toarray()
+#1 col france 2 col ger snd 3 col spain
+X = X[:,1:] #france col removed to avoid dummy variable trap
+
+#spliting the data
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.2)
+
+#feature scalling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#part 2 ANN building
