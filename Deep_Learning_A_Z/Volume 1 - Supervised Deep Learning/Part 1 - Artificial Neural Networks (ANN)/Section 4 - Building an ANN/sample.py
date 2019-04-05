805c5690dc21f69608ad560b2fdf408ce43eb319
skagnihotri <skagnihotri1@gmail.com>
2019-04-06 01:20:31 +0530

    working on ann

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/sample.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/sample.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 #dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:,3:-1].values
-y = dataset.iloc[:,-1].values
+y = dataset.iloc[:,13].values
 
 #categories
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
@@ -38,4 +38,40 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
 #part 2 ANN building
+
+#lib and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+#initialising ann
+classifier = Sequential()
+
+#input and first hidden layer
+classifier.add(Dense(units=6 ,activation='relu' ,input_dim=11))
+
+#second hidden layer
+classifier.add(Dense(units=6,activation='relu'))
+
+#output layer
+classifier.add(Dense(units=1,activation='sigmoid'))
+
+#compiling ann
+classifier.compile(optimizer='adam',
+                   loss='binary_crossentropy',
+                   metrics=['accuracy'])
+
+#fitting ann to training set
+classifier.fit(X_train,y_train,batch_size=10,epochs=100)
+
+
+#part 3
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
