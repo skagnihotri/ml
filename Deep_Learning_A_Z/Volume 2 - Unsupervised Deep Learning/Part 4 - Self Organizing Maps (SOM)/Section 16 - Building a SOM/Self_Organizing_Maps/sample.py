6d11da91537c26eab7f3341c83005d3c0a5e8363
skagnihotri <skagnihotri1@gmail.com>
2019-06-01 21:13:31 +0530

    som completed

--- Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organizing_Maps/sample.py
+++ Deep_Learning_A_Z/Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Self_Organizing_Maps/sample.py
@@ -20,4 +20,34 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler()
 X = sc.fit_transform(X)
 
-#som
+#fitting som
+from minisom import MiniSom
+som = MiniSom(x=10, y=10,
+              input_len=15, 
+              sigma=1.0,
+              learning_rate=0.5)
+som.random_weights_init(X)
+som.train_random(data=X, num_iteration=100)
+
+#visualising the som
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+marker = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0]+0.5,
+         w[1]+0.5,
+         marker[y[i]],
+         markeredgecolor= colors[y[i]],
+         markerfacecolor= 'None',
+         markersize= 10,
+         markeredgewidth= 2)
+show()
+
+#finding fraouds
+mapping = som.win_map(X)
+frauds = np.concatenate((mapping[(8,1)], mapping[(6,6)]), axis= 0)
+frauds = sc.inverse_transform(frauds)
